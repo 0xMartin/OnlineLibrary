422513178b7100497f8ea6b71ddd84fb09242828
0xMartin <martin.krcma1@gmail.com>
2022-11-12 13:32:23 +0100

    Base project code

---
+++ kod/LibraryApp/src/main/java/cz/utb/fai/LibraryApp/LibraryAppApplication.java
@@ -0,0 +1,13 @@
+package cz.utb.fai.LibraryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppApplication.class, args);
+	}
+
+}
---
+++ kod/LibraryApp/src/test/java/cz/utb/fai/LibraryApp/LibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.utb.fai.LibraryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
